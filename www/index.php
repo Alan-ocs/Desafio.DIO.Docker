ce7cecd9f1a23aa5b231506312a64cba43ec428b
Alan Silva <alan.ocs@gmail.com>
2022-08-02 00:03:52 -0300

    Updates

--- www/index.php
+++ www/index.php
@@ -13,9 +13,9 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "192.168.15.6";
 $username = "root";
-$password = "Senha123";
+$password = "mauFJcuf5dhRMQrjj";
 $database = "meubanco";
 
 // Criar conexão
